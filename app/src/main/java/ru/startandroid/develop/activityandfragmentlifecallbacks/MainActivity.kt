66e4f58008ae84a65a06e97431c37a848435dd31
Baudi2 <60889423+Baudi2@users.noreply.github.com>
2021-10-21 15:30:56 +0300

    Activity lifecycle callbacks added to log

--- app/src/main/java/ru/startandroid/develop/activityandfragmentlifecallbacks/MainActivity.kt
+++ app/src/main/java/ru/startandroid/develop/activityandfragmentlifecallbacks/MainActivity.kt
@@ -2,10 +2,53 @@ package ru.startandroid.develop.activityandfragmentlifecallbacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MY_LOG", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MY_LOG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MY_LOG", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MY_LOG", "onRestart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Log.d("MY_LOG", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d("MY_LOG", "onRestoreInstanceState")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MY_LOG", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MY_LOG", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MY_LOG", "onDestroy")
     }
 }
