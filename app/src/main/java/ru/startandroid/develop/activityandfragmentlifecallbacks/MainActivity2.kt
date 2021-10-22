04ec9f122323ee93f5a078ca5dc32785e42a2ce0
Baudi2 <60889423+Baudi2@users.noreply.github.com>
2021-10-22 12:30:21 +0300

    Two activities lifecycle callbacks logged

--- app/src/main/java/ru/startandroid/develop/activityandfragmentlifecallbacks/MainActivity2.kt
+++ app/src/main/java/ru/startandroid/develop/activityandfragmentlifecallbacks/MainActivity2.kt
@@ -1,60 +1,55 @@
 package ru.startandroid.develop.activityandfragmentlifecallbacks
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d("MY_LOG", "onCreate - ACTIVITY ONE")
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            startActivity(Intent(this, MainActivity2::class.java))
-        }
+        setContentView(R.layout.activity_main2)
+        Log.d("MY_LOG", "onCreate - ACTIVITY TWO")
     }
+
 
     override fun onStart() {
         super.onStart()
-        Log.d("MY_LOG", "onStart - ACTIVITY ONE")
+        Log.d("MY_LOG", "onStart - ACTIVITY TWO")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("MY_LOG", "onResume - ACTIVITY ONE")
+        Log.d("MY_LOG", "onResume - ACTIVITY TWO")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("MY_LOG", "onRestart - ACTIVITY ONE")
+        Log.d("MY_LOG", "onRestart - ACTIVITY TWO")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        Log.d("MY_LOG", "onRestoreInstanceState - ACTIVITY ONE")
+        Log.d("MY_LOG", "onRestoreInstanceState - ACTIVITY TWO")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("MY_LOG", "onPause - ACTIVITY ONE")
+        Log.d("MY_LOG", "onPause - ACTIVITY TWO")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("MY_LOG", "onStop - ACTIVITY ONE")
+        Log.d("MY_LOG", "onStop - ACTIVITY TWO")
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         super.onSaveInstanceState(outState, outPersistentState)
-        Log.d("MY_LOG", "onSaveInstanceState - ACTIVITY ONE")
+        Log.d("MY_LOG", "onSaveInstanceState - ACTIVITY TWO")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("MY_LOG", "onDestroy - ACTIVITY ONE")
+        Log.d("MY_LOG", "onDestroy - ACTIVITY TWO")
     }
 }
